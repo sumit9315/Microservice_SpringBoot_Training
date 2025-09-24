edeeb75e29ed57bd1dc4898b21ed4a450609e0a9
sumit9315 <sumit.202ps026@nitk.edu.in>
2025-09-24 13:33:03 +0530

    unit testing for post request

--- ProductCRUDAPI/src/test/java/com/example/productapi/controllers/ProductsControllerTest.java
+++ ProductCRUDAPI/src/test/java/com/example/productapi/controllers/ProductsControllerTest.java
@@ -1,6 +1,7 @@
 package com.example.productapi.controllers;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -8,6 +9,7 @@ import org.junit.jupiter.api.Test;
 //import org.mockito.internal.matchers.GreaterThan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.greaterThan;
@@ -23,4 +25,26 @@ public class ProductsControllerTest {
 		mockmvc.perform(get("/products")).andExpect(status().isOk()).andExpect(jsonPath("$.length()",greaterThan(5)));
 
 	}
+	
+	@Test
+	void testSingleProduct()throws Exception{
+		mockmvc.perform(get("/products/3")).andExpect(status().isNotFound());
+	}
+	
+	@Test
+	void testAddProduct() throws Exception{
+		String newProduct="""
+				
+				{
+				"name":"macbook air pro",
+				"price":67849039,
+				"category":"Laptop"
+	}
+	""";
+		mockmvc.perform(post("/products")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(newProduct))
+		.andExpect(status().isCreated());
+	}
+	
 }
